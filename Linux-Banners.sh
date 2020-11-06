3f27e470bc1ff7881b9583b8a964ef4a46df140b
Felipe Tapia <70113570+Drakiat@users.noreply.github.com>
2020-11-06 12:36:03 -0600

    Add files via upload

---
+++ Linux-Banners.sh
@@ -0,0 +1,14 @@
+echo Input banner:
+read banner
+
+chown root:root /etc/motd
+chmod 644 /etc/motd
+echo $banner > /etc/motd
+  
+chown root:root /etc/issue
+chmod 644 /etc/issue
+echo $banner > /etc/issue
+  
+chown root:root /etc/issue.net
+chmod 644 /etc/issue.net
+echo $banner > /etc/issue.net
---
+++ splunkforwarder.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo Enter IP of Splunk server:
+read ip
+wget -O splunkforwarder.tgz 'https://tinyurl.com/l1n-sp1-for'
+#curl -Lo splunkforwarder.tgz 'https://tinyurl.com/l1n-sp1-for'
+tar -xvzf splunkforwarder.tgz -C /opt
+cd /opt/splunkforwarder/bin
+./splunk start --accept-license
+./splunk enable boot-start
+./splunk add forward-server $ip:9997
+./splunk add monitor /var/log/
+./splunk restart
+
