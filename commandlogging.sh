d85e73938542b5072c4a94fe5c807e095349b31a
Felipe Tapia <70113570+Drakiat@users.noreply.github.com>
2020-11-11 20:56:20 -0600

    Update commandlogging.sh

--- commandlogging.sh
+++ commandlogging.sh
@@ -8,8 +8,8 @@ fi
 wget -qO- https://raw.githubusercontent.com/Drakiat/Blue-Team-Scripts/main/sec2.txt
 echo ""
 echo ""
-echo "export PROMPT_COMMAND='RETRN_VAL=$?;logger -p local6.debug "$(whoami) [$$]: $(history 1 | sed "s/^[ ]*[0-9]\+[ ]*//" ) [$RETRN_VAL]"'
-" >> /etc/bash.bashrc
+#This line is to fix
+#echo "export PROMPT_COMMAND='RETRN_VAL=$?;logger -p local6.debug "$(whoami) [$$]: $(history 1 | sed "s/^[ ]*[0-9]\+[ ]*//" ) [$RETRN_VAL]"'" >> /etc/bash.bashrc
 echo "local6.*    /var/log/commands.log" > /etc/rsyslog.d/bash.conf
 sed -i "/messages/a /var/log/commands.log" /etc/logrotate.d/rsyslog
 sudo service rsyslog restart
