3e258ef2476f1ead6a9bd5c0bbf0736a870fafd5
Drakiat <felipetapia@hotmail.fr>
2020-11-12 16:11:40 -0600

    added helper file

--- commandlogging.sh
+++ commandlogging.sh
@@ -8,8 +8,6 @@ fi
 wget -qO- https://raw.githubusercontent.com/Drakiat/Blue-Team-Scripts/main/sec2.txt
 echo ""
 echo ""
-#This line is to fix
-#echo >> /etc/bash.bashrc
 if [ ! -f commandlogging-helper.txt ]; then
     wget -o commandlogging-helper.txt https://raw.githubusercontent.com/Drakiat/Blue-Team-Scripts/main/
 fi
@@ -18,3 +16,4 @@ echo $line >> /etc/bash.bashrc
 echo "local6.*    /var/log/commands.log" > /etc/rsyslog.d/bash.conf
 sed -i "/messages/a /var/log/commands.log" /etc/logrotate.d/rsyslog
 sudo service rsyslog restart
+echo "Command log is set up! All users commands will be logged at /var/log/commands.log"
