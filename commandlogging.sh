43f34db8dd93f867a1aa9270a6fc2e91bc25eafb
Drakiat <felipetapia@hotmail.fr>
2020-11-12 16:05:49 -0600

    added helper file

--- commandlogging.sh
+++ commandlogging.sh
@@ -9,7 +9,12 @@ wget -qO- https://raw.githubusercontent.com/Drakiat/Blue-Team-Scripts/main/sec2.
 echo ""
 echo ""
 #This line is to fix
-#echo "export PROMPT_COMMAND='RETRN_VAL=$?;logger -p local6.debug "$(whoami) [$$]: $(history 1 | sed "s/^[ ]*[0-9]\+[ ]*//" ) [$RETRN_VAL]"'" >> /etc/bash.bashrc
+#echo >> /etc/bash.bashrc
+if [ ! -f commandlogging-helper.txt ]; then
+    wget -o commandlogging-helper.txt https://raw.githubusercontent.com/Drakiat/Blue-Team-Scripts/main/
+fi
+line=$(head -n 1 commandlogging-helper.txt)
+echo $line >> /etc/bash.bashrc
 echo "local6.*    /var/log/commands.log" > /etc/rsyslog.d/bash.conf
 sed -i "/messages/a /var/log/commands.log" /etc/logrotate.d/rsyslog
 sudo service rsyslog restart
