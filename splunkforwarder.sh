82ccba81e1be80da56dc35a8cd801ad3cfe55b43
Felipe Tapia <70113570+Drakiat@users.noreply.github.com>
2020-12-28 20:27:26 -0600

    Update splunkforwarder.sh

--- splunkforwarder.sh
+++ splunkforwarder.sh
@@ -10,15 +10,6 @@ echo ""
 echo ""
 echo Enter IP of Splunk server:
 read ip
-#wget -O splunkforwarder.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.0&product=universalforwarder&filename=splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz&wget=true'
-curl -Lo splunkforwarder.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.0&product=universalforwarder&filename=splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz&wget=true'
-tar -xvzf splunkforwarder.tgz -C /opt
-cd /opt/splunkforwarder/bin
-./splunk start --accept-license
-./splunk enable boot-start
-./splunk add forward-server $ip:9997
-./splunk add monitor /var/log/
-./splunk restart
 read -p "Do you want iptables rules to be added? (Y/N)" -n 1 -r
 echo  
 if [[ $REPLY =~ ^[Yy]$ ]]
@@ -28,4 +19,15 @@ then
     iptables -I INPUT -p tcp --sport 9997 -s $ip -j ACCEPT
     iptables -I OUTPUT -p tcp --dport 9997 -d $ip -j ACCEPT
 fi
+#wget -O splunkforwarder.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.0&product=universalforwarder&filename=splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz&wget=true'
+curl -Lo splunkforwarder.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.0&product=universalforwarder&filename=splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz&wget=true'
+tar -xvzf splunkforwarder.tgz -C /opt
+cd /opt/splunkforwarder/bin
+./splunk start --accept-license
+./splunk enable boot-start
+./splunk add forward-server $ip:9997
+./splunk add monitor /var/log/
+./splunk restart
+echo "iptables -I INPUT -p tcp --sport 9997 -s $ip -j ACCEPT"
+echo "iptables -I OUTPUT -p tcp --dport 9997 -d $ip -j ACCEPT"
 
