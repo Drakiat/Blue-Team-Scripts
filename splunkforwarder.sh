0dbbd52b1dd680d34a708970a2bba9276c22d327
Drakiat <felipetapia@hotmail.fr>
2021-01-13 18:57:11 -0600

    updated

--- splunkforwarder.sh
+++ splunkforwarder.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+# ---------------------------------------------------------------------------------------------------------------------
+#@(#)$Id$
+#title  :splunkforwarder.sh
+#description:   this script is designed to automate the installation and configuration of Splunk Universal Fowarder on Linux machines
+#usage: ./splunkforwarder.sh
+# ---------------------------------------------------------------------------------------------------------------------
 if [[ $UID != 0 ]]; then
     echo "Please run this script with sudo:"
     echo "sudo $0 $*"
     exit 1
 fi
 #ASCII :)
+echo "test"
 FILE=sec2.txt
 if [ -f "$FILE" ]; then
     cat $FILE
@@ -26,8 +33,17 @@ then
     iptables -I OUTPUT -p tcp --dport 9997 -d $ip -j ACCEPT
 fi
 #wget -O splunkforwarder.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.0&product=universalforwarder&filename=splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz&wget=true'
-curl -Lo splunkforwarder.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.0&product=universalforwarder&filename=splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz&wget=true'
-tar -xvzf splunkforwarder.tgz -C /opt
+#curl -Lo splunkforwarder.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.0&product=universalforwarder&filename=splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz&wget=true'
+FILE=splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz
+if [ -f "$FILE" ]; then
+    tar -xvzf $FILE -C /opt
+    echo "Splunk installer found..."
+else
+    echo "Fetching Splunk installer from the internet..."
+    curl -Lo splunkforwarder.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.0&product=universalforwarder&filename=splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz&wget=true'
+    tar -xvzf splunkforwarder.tgz -C /opt
+
+fi
 cd /opt/splunkforwarder/bin
 ./splunk start --accept-license
 ./splunk enable boot-start
