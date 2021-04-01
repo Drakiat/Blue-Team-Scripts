88c265c0ca48eb3ada3d407f39074f53b09688df
Felipe Tapia <70113570+Drakiat@users.noreply.github.com>
2021-03-31 20:43:21 -0500

    Update addusers.sh
    replaced sudo by UID 0 check

--- addusers.sh
+++ addusers.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
 # ASCII Cause why not ;)
 echo ""
 echo ""
@@ -13,10 +16,10 @@ echo ""
 # Parse through CSV file of usernames,passwords
 while IFS=, read -r user pass; do
 	# Add user
-	sudo su -c "useradd $user -s /bin/bash -m"
+	su -c "useradd $user -s /bin/bash -m"
 	echo "Added user: $user ."
 
 	# Set user pass
-	echo "$user:$pass" | sudo chpasswd
+	echo "$user:$pass" | chpasswd
 	echo "Gave $user a password: $pass .";
 done < users.csv
