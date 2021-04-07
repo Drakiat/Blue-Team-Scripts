7f9cbe5aa05c60ee94194b508771bf136547eef1
Drakiat <felipetapia@hotmail.fr>
2021-04-06 19:30:27 -0500

    yessir

--- getcap.sh
+++ getcap.sh
@@ -1,4 +1,9 @@
 getcap -r / 2>/dev/null > cap.txt
 while read p; do
-  echo "$p" | awk '{print $1;}'
+  echo "$p" | awk '{print $1;}' >capbin.txt
 done <cap.txt
+
+while read b; do
+  echo "$b"
+  setcap -r $b
+done <capbin.txt
