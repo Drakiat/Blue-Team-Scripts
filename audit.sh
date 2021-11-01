ee21a7f3a942e9284b800058b4b1e81da141768e
Felipe Tapia <70113570+Drakiat@users.noreply.github.com>
2021-11-01 09:33:27 -0500

    Update audit.sh

--- audit.sh
+++ audit.sh
@@ -13,7 +13,7 @@ echo "hostname: "`hostname` >audit.txt
 #internal IP
 echo "Internal IP: " `ip a | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'` >> audit.txt
 #External IP
-echo -e "Chceking external IP, might take a few minutes \e[5m...\e[0m"
+echo -e "Chceking external IP, might take a few minutes(not always accurate) \e[5m...\e[0m"
 echo "External IP: "`dig +short myip.opendns.com @resolver1.opendns.com`>>audit.txt
 echo -e "\e[34mDone!\e[0m"
 #OS version
@@ -22,13 +22,18 @@ echo "OS version: " `uname -mrs`>> audit.txt
 read -p "Purpose of this box(A few words is enough): " purpose
 echo "Description: " $purpose >> audit.txt
 #Critical Application(s)
-read -p "Critical Application(separate with a comma if multiple): " app
+read -p "Critical Applications(ex: Prestashop,VSFTPD...)(separate with a comma if multiple): " app
 echo "Critical Application(s): "$app>>audit.txt
 #List of Admin/User/Service Accounts
-echo "List of Admin/User/Service Accounts:" `grep '^sudo:.*$' /etc/group | cut -d: -f4|tr '\n' ' '^C && grep '^wheel:.*$' /etc/group | cut -d: -f4
+echo "List of Admin Accounts:" `grep '^sudo:.*$' /etc/group | cut -d: -f4|tr '\n' ' '^C && grep '^wheel:.*$' /etc/group | cut -d: -f4
 `>>audit.txt
 #List of Services Running
+read -p "Services Running(ex: HTTP,SSH,DNS...)(separate with a comma if multiple): " services
+echo "Services Running: "$services>>audit.txt
 #List of Open Ports
+ss -tulnp
+read -p "Open Ports(ex: 80,22...)(separate with a comma if multiple): " ports
+echo "Open Ports: "$ports>>audit.txt
 echo ""
 echo ""
 cat audit.txt
